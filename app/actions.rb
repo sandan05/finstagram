79acc680e94d550aec7614567274b753b51a9f04
sandan05 <tolulopesanusi05@gmail.com>
2020-11-26 04:57:07 +0000

    add user and FinstagramPost models

--- app/actions.rb
+++ app/actions.rb
@@ -1,33 +1,56 @@
 #get '/' do
  #   File.read(File.join('app/views', 'index.html'))
 #end
-get '/' do
-username = "sharky_j"
-avater_url = "http://naserca.com/images/sharky_j.jpg"
-photo_url = "http://naserca.com/images/shark.jpg"
-time_ago_in_minutes = 156
-like_count = 0
-comment_count = 1
-comments = ["sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"]
-#if the time_ago_in_minutes is greater than 60
-if time_ago_in_minutes > 60
-    #return this string
-    "more than an hour ago"
-    #if it's equal to 60
-elsif time_ago_in_minutes == 60
-"an hour"
-    #if it's less than 60
-    elsif time_ago_in_minutes <= 1
-        "Just a moment ago"
-else
-    #return this instead
-    "less than an hour"
-end
 
-def humanized_time_ago(minute_num)
-    if minute_num >= 60
-        "#{minute_num /60} hours ago"
+def humanized_time_ago(time_ago_in_minutes)
+    if time_ago_in_minutes >= 60
+        "#{time_ago_in_minutes /60} hours ago"
     else
-        "#{minute_num} minutes ago"
+        "#{time_ago_in_minutes} minutes ago"
     end
 end
+
+get '/' do
+    @finstagram_post_shark = {
+username: "sharky_j",
+avatar_url: "http://naserca.com/images/sharky_j.jpg",
+photo_url: "http://naserca.com/images/shark.jpg",
+humanized_time_ago: humanized_time_ago(15),
+like_count: 0,
+comment_count: 1,
+comments: [{
+    username:"sharky_j",
+text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"}]
+    }
+@finstagram_post_whale = {
+username: "kirk_whalum",
+avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+photo_url: "http://naserca.com/images/whale.jpg",
+humanized_time_ago: humanized_time_ago(65),
+like_count: 0,
+comment_count: 1,
+comments: [{
+    username: "kirk_whalum",
+    text: "#weekendvibes"
+}]
+}
+
+@finstagram_post_marlin ={
+    username: "marlin_peppa",
+    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+    photo_url: "http://naserca.com/images/marlin.jpg",
+    humanized_time_ago: humanized_time_ago(190),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+        username: "marlin_peppa",
+        text: "lunchtime! ;)"
+    }]
+}
+
+@finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+erb(:index)
+end
+ 
+
+
